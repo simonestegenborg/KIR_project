a704d1bb4c1af37d236d981200cfe9892a95c957
simonestegenborg <121563506+simonestegenborg@users.noreply.github.com>
2023-01-30 15:35:42 +0100

    Update Ab_curve.R

--- Ab_curve.R
+++ Ab_curve.R
@@ -1,17 +1,17 @@
 library(alakazam)
 
-bf1 <- kir@meta.data %>% 
+bf1 <- KIR.subset@meta.data %>% 
   filter(buffy_coat == 'BC351') %>%
   filter(!is.na(TRB_clone)) %>%
-  mutate(groups = recode(HTO_classification, 'S1'='S1', 'S2'='S2_S3', 'S3'='S2_S3', 'S5'='S5'))
+  mutate(groups = recode(HTO_classification, 'S1'='S1', 'S2'='S2', 'S3'='S3', 'S5'='S5'))
 
 curve1 <- estimateAbundance(bf1, group="groups", ci=0.95, nboot=100, clone="TRB_clone", )
 
-bf2 <- kir@meta.data %>% 
+bf2 <- KIR.subset@meta.data %>% 
   filter(buffy_coat == 'BC357') %>%
   filter(!is.na(TRB_clone)) %>%
-  mutate(groups = recode(HTO_classification, 'S6'='S6_S7', 'S7'='S6_S7', 'S8'='S8', 'S10'='S10'))
+  mutate(groups = recode(HTO_classification, 'S6'='S6', 'S7'='S7', 'S8'='S8', 'S10'='S10'))
 
 curve2 <- estimateAbundance(bf2, group="groups", ci=0.95, nboot=100, clone="TRB_clone", )
 
-plot(curve1, legend_title="Sample") + plot(curve2, legend_title="Sample")
+plot(curve1, legend_title="Sample") + plot(curve2, legend_title="Sample", )
